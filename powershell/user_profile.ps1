a5794d78426391f50b6399261950024c9c973089
Tino Tonitini <georgepike0@gmail.com>
2025-06-20 11:10:00 -0400

    Trim EOF newline from Powershell profile

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,4 +1,4 @@
-﻿# === dev-setup profile ==================================================
+# === dev-setup profile ==================================================
 # Starship prompt
 if (Get-Command starship -ErrorAction SilentlyContinue) {
     Invoke-Expression ((& starship init powershell) -join "`n")
