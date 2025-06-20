ec9e2a9e78b92456c01acf71b375d1d25b09c29c
Tino Tonitini <georgepike0@gmail.com>
2025-06-20 11:03:27 -0400

    Fix theme path initialization

--- tablet-config/powershell/Microsoft.PowerShell_profile.ps1
+++ tablet-config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
 # Initialize Oh My Posh
-Invoke-Expression (& { (oh-my-posh init powershell --config "$env:USERPROFILE/custom_tokyo.omp.json" | Out-String) })
+$ThemePath = Join-Path $PSScriptRoot "..\oh-my-posh\custom_tokyo.omp.json"
+Invoke-Expression (& { (oh-my-posh init powershell --config $ThemePath | Out-String) })
 
 # Initialize Zoxide
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
