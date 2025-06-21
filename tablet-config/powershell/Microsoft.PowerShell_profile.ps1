c682a9cffbd9c704626446fadd67bcfb66edac63
Tino Tonitini <georgepike0@gmail.com>
2025-06-20 22:48:36 -0400

    remove oh my posh init

--- tablet-config/powershell/Microsoft.PowerShell_profile.ps1
+++ tablet-config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,3 @@
-# Initialize Oh My Posh if available
-$ThemePath = Join-Path $PSScriptRoot "..\oh-my-posh\custom_tokyo.omp.json"
-if (Get-Command oh-my-posh -ErrorAction SilentlyContinue) {
-    Invoke-Expression (& { (oh-my-posh init powershell --config $ThemePath | Out-String) })
-}
-
 # Initialize Zoxide if available
 if (Get-Command zoxide -ErrorAction SilentlyContinue) {
     Invoke-Expression (& { (zoxide init powershell | Out-String) })
