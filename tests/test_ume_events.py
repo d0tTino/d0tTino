68cad43fef377e30c06c5d7e2cb05f6660f2185f
Tino Tonitini <georgepike0@gmail.com>
2025-07-30 21:35:59 -0400

    Add JetStream helpers

--- tests/test_ume_events.py
+++ tests/test_ume_events.py
@@ -15,6 +15,7 @@ class FakeNATS:
         self.published = []
         self.connected = []
         self.drained = False
+        self.js = FakeJS(self)
 
     async def connect(self, servers=None):
         self.connected.append(servers)
@@ -31,6 +32,9 @@ class FakeNATS:
     async def subscribe(self, subject):
         self.subscribed = subject
         return FakeSub()
+
+    def jetstream(self):
+        return self.js
 
 
 class FakeSub:
@@ -49,6 +53,36 @@ class FakeSub:
 class FakeMsg:
     def __init__(self, data):
         self.data = data
+
+    async def ack(self):
+        self.acked = True
+
+
+class FakePullSub:
+    def __init__(self):
+        self.messages = [FakeMsg(b"{\"b\": 2}")]
+
+    async def fetch(self, batch):
+        msgs = self.messages[:batch]
+        self.messages = self.messages[batch:]
+        return msgs
+
+    async def unsubscribe(self):
+        self.unsub = True
+
+
+class FakeJS:
+    def __init__(self, nc):
+        self.nc = nc
+        self.published = []
+
+    async def publish(self, subject, data):
+        self.published.append((subject, data))
+
+    async def pull_subscribe(self, subject, durable="ume_iter"):
+        self.pull_subject = subject
+        self.durable = durable
+        return FakePullSub()
 
 
 def test_publish_event_sync(monkeypatch):
@@ -78,4 +112,33 @@ async def test_iter_events(monkeypatch):
     gen = events.iter_events()
     event = await gen.__anext__()
     assert event == {"a": 1}
+    await gen.aclose()
+
+
+@pytest.mark.asyncio
+async def test_publish_event_js(monkeypatch):
+    fake = FakeNATS()
+
+    async def fake_connect(url=events.DEFAULT_NATS_URL):
+        return fake
+
+    monkeypatch.setattr(events, "_connect", fake_connect)
+    success = await events.publish_event_js("js", {"y": 3})
+    assert success is True
+    assert fake.js.published == [
+        (events.DEFAULT_SUBJECT, b'{"name": "js", "y": 3}')
+    ]
+
+
+@pytest.mark.asyncio
+async def test_iter_events_js(monkeypatch):
+    fake = FakeNATS()
+
+    async def fake_connect(url=events.DEFAULT_NATS_URL):
+        return fake
+
+    monkeypatch.setattr(events, "_connect", fake_connect)
+    gen = events.iter_events_js()
+    event = await gen.__anext__()
+    assert event == {"b": 2}
     await gen.aclose()
